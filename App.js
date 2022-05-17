d7ae3332453547c9fdd2b3f3276c020888d35c8b
BILLEL <billel.bahani@gmail.com>
2022-05-17 16:26:06 +0100

    start

--- App.js
+++ App.js
@@ -4,8 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          width: 100,
+          height: 100,
+          shadowColor: 'grey',
+          shadowOffset: { width: 0, height: 10 },
+          shadowOpacity: 1,
+          shadowRadius: 10,
+          elevation: 90,
+          paddingHorizontal: 10,
+        }}
+      >
+        <Text>Bleu Box</Text>
+      </View>
     </View>
   );
 }
